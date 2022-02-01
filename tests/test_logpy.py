0348ebd2104ec9f2eab2ec73a1bbfdaa11949f7e
Los-had <githubtestefinal@gmail.com>
2022-02-01 18:35:30 -0200

    small changes

--- tests/test_logpy.py
+++ tests/test_logpy.py
@@ -1,5 +1,22 @@
-from logpy import __version__
+from logpy import __version__, Logger
 
+def test_critical():
+    pass
+
+def test_error():
+    pass
+
+def test_warning():
+    pass
+
+def test_info():
+    pass
+
+def test_success():
+    pass
+
+def test_log():
+    pass
 
 def test_version():
     assert __version__ == '0.0.1'

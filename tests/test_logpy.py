68005ad9b00b84eed83f424d0862e2002887b148
Los-had <githubtestefinal@gmail.com>
2022-02-03 21:13:04 -0200

    test for the new line break functionallity

--- tests/test_logpy.py
+++ tests/test_logpy.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import List
 from logpy import __version__
 import os
 
@@ -35,8 +36,37 @@ def test_log(console):
 
 def test_log_save(console):
     console.info("hi")
+
     assert os.path.exists("./logs.log")
 
+
+def test_line_breaks_in_log_file(console):
+    def open_log_file() -> List[str]:
+        try:
+            f = open("./logs.log", "r")
+            ctx = f.readlines()
+
+            return ctx
+        finally:
+            f.close()
+
+    console.critical("1")
+    console.error("2")
+    console.warning("3")
+    console.info("4")
+    console.success("5")
+    
+    try:
+        line_count = open_log_file()
+    except IOError:
+        console.init()
+        line_count = open_log_file()
+    except FileNotFoundError:
+        console.init()
+        line_count = open_log_file()
+
+    assert len(line_count) > 1
+    assert os.path.exists("./logs.log")
 
 def test_version():
     assert __version__ == "0.0.1"
